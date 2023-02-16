7178fab7923e2723a7f389e7bf04ab48905bb5fd
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-16 22:55:22 +0600

    create project context updated

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useProjectsContext } from "../hooks/useProjectsContext";
 
 const ProjectForm = () => {
   const [title, setTitle] = useState("");
@@ -9,11 +10,13 @@ const ProjectForm = () => {
   const [dev, setDev] = useState("");
   const [error, setError] = useState(null);
 
+  const { dispatch } = useProjectsContext();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // data
-    const project = { title, tech, budget, duration, manager, dev };
+    const projectObj = { title, tech, budget, duration, manager, dev };
 
     // post req
     const res = await fetch("http://localhost:5000/api/projects", {
@@ -21,7 +24,7 @@ const ProjectForm = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(project),
+      body: JSON.stringify(projectObj),
     });
     const json = await res.json();
 
@@ -39,8 +42,7 @@ const ProjectForm = () => {
       setManager("");
       setDev("");
       setError(null);
-
-      console.log("New project has been added to the db", json);
+      dispatch({ type: "CREATE_PROJECT", payload: json });
     }
   };
 
